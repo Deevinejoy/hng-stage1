2ade10f1947c3b136e0867ee9cd8bf2e6a376ff9
Deevinejoy <akindivinejoy@gmail.com>
2024-07-02 05:12:44 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const time = document.querySelector('.current-time')
+const day = document.querySelector('.current-day')
+const dayofWeek =['sunday', 'monday','tuesday', 'wednessday', 'thursday', 'friday', 'saturday']
+
+const current = new Date()
+const currentTime = current.toUTCString()
+const currentDay = current.getUTCDay()
+time.textContent=`Time:${currentTime.slice(16, -3)} UTC`
+day.textContent = `Day:${dayofWeek[currentDay].toString()}`
+
+//const weekDay = `${currentTime.slice(0,3)}sday`
+//console.log(weekDay)
+
+
+
+
